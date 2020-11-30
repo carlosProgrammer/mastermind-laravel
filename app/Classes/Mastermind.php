da7eefac52757d88a71b8367ad51986bb28059c4
carlosProgrammer <kkobtk@gmail.com>
2020-11-30 10:06:01 -0400

    Created Mastermind class

---
+++ app/Classes/Mastermind.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Classes;
+
+use App\Traits\ColorsDSGenerator;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+
+class Mastermind 
+{
+
+}
+
+?>
